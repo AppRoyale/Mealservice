d75763c2edc46b226b7b0ccdc356311675ef78ce
AppRoyale <andre3k@gmx.de>
2013-08-01 12:03:21 +0200

    calendar added

---
+++ Resources/calendar/bootstrap-formhelpers-datepicker.deu.js
@@ -0,0 +1,24 @@
+  var BFHMonthsList = [
+ 	'Januar',
+ 	'Februar',
+ 	'März',
+ 	'April',
+ 	'Mai',
+ 	'Juni',
+ 	'Juli',
+ 	'August',
+ 	'September',
+ 	'Oktober',
+ 	'November',
+ 	'Dezember'
+ ]
+ 
+ var BFHDaysList = [
+ 	"SO",
+ 	"MO",
+ 	"DI",
+ 	"Mi",
+ 	"Do",
+ 	"FR",
+ 	"SA"
+ ]
--- dist/win32/Siemens/Resources/js/app.js
+++ dist/win32/Siemens/Resources/js/app.js
@@ -1,8 +1,41 @@
+/**
+ * User: Andre Schiemann
+ * Date: 31.07.13
+ * Time: 20:05
+ *
+ * Never change a running system.
+ *
+ * Get Updates from http://builds.emberjs.com/
+ */
 var App = Ember.Application.create();
 
+App.Router.map(function() {
+    this.resource('inventory');
+    this.resource('login');
+    this.resource('menu');
+});
+
 App.IndexRoute = Ember.Route.extend({
+    redirect: function() {
+        this.transitionTo('login');
+    }
+});
+
+/**
+ * Set the active class of navbar
+ */
+App.ApplicationView = Ember.View.extend({
+    currentPathDidChange: function() {
+        Ember.run.next( this, function() {
+            this.$("ul.nav li:has(>a.active)").addClass('active');
+            this.$("ul.nav li:not(:has(>a.active))").removeClass('active');
+        });
+    }.observes('controller.currentPath')
+});
+
+App.InventoryRoute = Ember.Route.extend({
     model: function(){
-        return Ember.Object.create({post: App.Post.find(), page: App.Page.find()});
+        return Ember.Object.create({vine: App.Vine.find(), meal: App.Meal.find()});
     }
 });
 
@@ -11,36 +44,170 @@ App.Store = DS.Store.extend({
     adapter: 'DS.FixtureAdapter'
 });
 
-App.Post = DS.Model.extend({
+App.Vine = DS.Model.extend({
     title: DS.attr('string'),
     description: DS.attr('string')
 });
 
-App.Page = DS.Model.extend({
+App.Meal = DS.Model.extend({
     title: DS.attr('string'),
     text: DS.attr('string')
 });
 
-App.Post.FIXTURES = [
+App.Vine.FIXTURES = [
     {
         id: 1,
-        title: "My super post",
-        description: "Lorem ipsum dolor sit amet..."
+        title: "Wein 1"
     }, {
-    id: 2,
-    title: "My super post",
-    description: "Lorem ipsum dolor sit amet..."
+        id: 2,
+        title: "Wein 2"
+}    , {
+        id: 3,
+        title: "Wein 3"
+    }, {
+        id: 4,
+        title: "Wein 4"
+    }, {
+        id: 5,
+        title: "Wein 5"
+    }, {
+        id: 6,
+        title: "Wein 6"
+    }, {
+        id: 7,
+        title: "Wein 7"
+    }, {
+        id: 8,
+        title: "Wein 8"
+    }, {
+        id: 9,
+        title: "Wein 9"
+    }, {
+        id: 10,
+        title: "Wein 10"
+    }, {
+        id: 11,
+        title: "Wein 11"
+    }, {
+        id: 12,
+        title: "Wein 12"
+    }, {
+        id: 13,
+        title: "Wein 13"
+    }, {
+        id: 14,
+        title: "Wein 14"
+    }, {
+        id: 15,
+        title: "Wein 15"
+    }, {
+        id: 16,
+        title: "Wein 16"
+    }, {
+        id: 17,
+        title: "Wein 17"
+    }, {
+        id: 18,
+        title: "Wein 18"
+    }, {
+        id: 19,
+        title: "Wein 19"
+    }, {
+        id: 20,
+        title: "Wein 20"
+    }, {
+        id: 21,
+        title: "Wein 21"
+    }, {
+        id: 22,
+        title: "Wein 22"
+    }, {
+        id: 23,
+        title: "Wein 23"
+    }, {
+        id: 24,
+        title: "Wein 24"
+    }, {
+        id: 25,
+        title: "Wein 25"
     }
 ];
 
-App.Page.FIXTURES = [
+App.Meal.FIXTURES = [
     {
         id: 1,
-        title: "My super page",
-        text: "Lorem ipsum dolor sit amet..."
+        title: "Speise 1"
     }, {
         id: 2,
-        title: "yolo",
-        text: "yolo"
+        title: "Speise 2"
+    }    , {
+        id: 3,
+        title: "Speise 3"
+    }, {
+        id: 4,
+        title: "Speise 4"
+    }, {
+        id: 5,
+        title: "Speise 5"
+    }, {
+        id: 6,
+        title: "Speise 6"
+    }, {
+        id: 7,
+        title: "Speise 7"
+    }, {
+        id: 8,
+        title: "Speise 8"
+    }, {
+        id: 9,
+        title: "Speise 9"
+    }, {
+        id: 10,
+        title: "Speise 10"
+    }, {
+        id: 11,
+        title: "Speise 11"
+    }, {
+        id: 12,
+        title: "Speise 12"
+    }, {
+        id: 13,
+        title: "Speise 13"
+    }, {
+        id: 14,
+        title: "Speise 14"
+    }, {
+        id: 15,
+        title: "Speise 15"
+    }, {
+        id: 16,
+        title: "Speise 16"
+    }, {
+        id: 17,
+        title: "Speise 17"
+    }, {
+        id: 18,
+        title: "Speise 18"
+    }, {
+        id: 19,
+        title: "Speise 19"
+    }, {
+        id: 20,
+        title: "Speise 20"
+    }, {
+        id: 21,
+        title: "Speise 21"
+    }, {
+        id: 22,
+        title: "Speise 22"
+    }, {
+        id: 23,
+        title: "Speise 23"
+    }, {
+        id: 24,
+        title: "Speise 24"
+    }, {
+        id: 25,
+        title: "Speise 25"
     }
 ];
